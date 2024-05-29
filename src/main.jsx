2a3fcd5272e91eb74b88c6340b78c6681af29fa3
QuangAnhTo <quangato072@gmail.com>
2024-05-29 17:25:05 +0700

    feat: Add Redux store configuration and Provider component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { LoginContext } from "./Context/CheckLogin.jsx";
+import { Provider } from "react-redux";
+import store from "./Store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <LoginContext>
-      <App />
-    </LoginContext>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <LoginContext>
+        <App />
+      </LoginContext>
+    </BrowserRouter>
+  </Provider>
 );
