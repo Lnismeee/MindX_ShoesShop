3dd4eb8a2ff6ffb2ecf339220703e2682134762d
QuangAnhTo <quangato072@gmail.com>
2024-06-06 23:54:20 +0700

    feat: add navlink to detail product

--- src/Page/ProductList/List/List.jsx
+++ src/Page/ProductList/List/List.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Card from "../../../Components/Card/Card";
 import "./index.css";
+import { NavLink } from "react-router-dom";
 const List = ({ data, setData }) => {
   return (
     <div className="list12">
       <div className="list">
-      {data.map((item, index) => {
-        if (item.hide == true)
-          return (
-            <div className="card1">
-              <Card data={item} key={item.id} />
-            </div>
-          );
-      })}
-    </div>
+        {data.map((item, index) => {
+          if (item.hide == true) {
+            return (
+              <div className="card1">
+                <NavLink to={`/products/${item._id}`}>
+                  <Card
+                    data={item}
+                    key={item.id}
+                  />
+                </NavLink>
+              </div>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 };
