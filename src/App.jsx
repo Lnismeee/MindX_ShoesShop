a270c942e8bf005992db871147e663f815882d4c
Lnismeee <langocltr02@gmail.com>
2024-06-14 23:09:13 +0700

    example2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Contact from "./Page/Contact";
 import { Routes, Route } from "react-router-dom";
 import Header from "./Page/theme/Header/Header";
 import Footer from "./Page/theme/Footer/Footer";
-import Example1 from "./Components/Example";
+import Example1 from "./Components/Example1";
 import Login from "./Page/Login";
 import UserPage from "./Page/UserPage";
 import { useDispatch } from "react-redux";
@@ -42,7 +42,7 @@ function App() {
         <Route path="/cart/checkout" element={<Checkout />} />
         <Route path="/news/detail" element={<NewsDetail />} />
       </Routes>
-      <Example1></Example1>
+      <Example1/>
       <Footer />
     </>
   );
