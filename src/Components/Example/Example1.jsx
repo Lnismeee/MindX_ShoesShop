b7668d7747b360be8154cede137d9be52d6f675a
Lnismeee <langocltr02@gmail.com>
2024-06-15 15:26:00 +0700

    sửa lại 1 số lỗi Example

--- src/Components/Example/Example1.jsx
+++ src/Components/Example/Example1.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./styles.css";
 import data from "./data";
-
+import { NavLink } from "react-router-dom";
 const Example1 = () => {
     return (
         <div id="example">
-            {data.map(({ name, icon }, index) => (
-                <div className="item" key={index}>
+            {data.map(({ name, icon, link }, index) => (
+                <NavLink to={link} target="_blank">
+                    <div className="item" key={index}>
                     <img src={icon} alt="" className="itemE" />
                     <p>{name}</p>
                 </div>
+                </NavLink>
             ))}
         </div>
     );
