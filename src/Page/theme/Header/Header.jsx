3fd0982fc024211c126a6fc760c605d984956671
QuangAnhTo <quangato072@gmail.com>
2024-06-15 16:53:33 +0700

    style: restyling header

--- src/Page/theme/Header/Header.jsx
+++ src/Page/theme/Header/Header.jsx
@@ -6,7 +6,7 @@ import Header_center from "./Header_center/Header_center";
 import Header_right from "./Header_right/Header_right";
 const Header = () => {
   return (
-    <div className="flex flex-row w-full bg-white z-20 justify-between items-center py-3 pr-5 fixed shadow-md ">
+    <div className="flex flex-row w-full bg-white z-20 justify-between items-center py-3 fixed shadow-md ">
       <Header_left />
       <Header_center />
       <Header_right />
